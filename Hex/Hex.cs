14f99458fc807c4a120c05ba60ea7e443b737a55
BabyPigman <babypigmanyt@gmail.com>
2025-08-17 16:38:41 +0200

    Initial commit

---
+++ Hex/Hex.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Hex;
+
+public class Hex
+{
+    internal static bool HadError = false;
+    
+    public static void Main(String[] args)
+    {
+        switch (args.Length)
+        {
+            case > 1:
+                Console.WriteLine("Usage: hex [script]");
+                break;
+            case 1:
+                RunFile(args[0]);
+                break;
+            default:
+                RunPrompt();
+                break;
+        }
+    }
+
+    private static void RunFile(String path)
+    {
+        byte[] bytes = File.ReadAllBytes(path);
+        string content = Encoding.Default.GetString(bytes);
+        Run(content);
+        
+        if (HadError) Environment.Exit(65);
+    }
+
+    private static void RunPrompt()
+    {
+        while (true)
+        {
+            Console.WriteLine("> ");
+            String? line = Console.ReadLine();
+            if (line == null) break;
+            Run(line);
+            HadError = false;
+        }
+    }
+
+    private static void Run(String source)
+    {
+        Lexer lexer = new Lexer();
+        List<Token> tokens = lexer.scanTokens();
+        
+        foreach (Token token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+
+    internal static void Error(int line, String message)
+    {
+        Report(line, "", message);
+    }
+
+    private static void Report(int line, String where, String message)
+    {
+        Console.WriteLine($"[line {line}] Error {where}: {message}");
+        hadError = true;
+    }
+}
---
+++ Hex/Token.cs
@@ -0,0 +1,22 @@
+﻿namespace Hex;
+
+public class Token
+{
+    private readonly TokenType _type;
+    private readonly String _lexeme;
+    private readonly Object _literal;
+    private readonly int _line;
+
+    Token(TokenType type, String lexeme, Object literal, int line)
+    {
+        this._type = type;
+        this._lexeme = lexeme;
+        this._literal = literal;
+        this._line = line;
+    }
+
+    public override string ToString()
+    {
+        return _type + " " + _lexeme + " " + _literal;
+    }
+}
